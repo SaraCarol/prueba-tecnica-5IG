0282ab3e7e7ec338cca26f7519f7db73d9f6ee34
SaraCarol <mendoza.saracarolina@gmail.com>
2025-08-04 15:21:04 -0500

    feat: Configurando rutas de la Pagina Home

--- task-3/src/App.jsx
+++ task-3/src/App.jsx
@@ -1,13 +1,19 @@
 
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+
 
 function App() {
   
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
       
-    </>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ task-3/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
+//import { useProducts } from '../contexts/ProductContext';
+
+const Home = () => {
+    //const { products, loading, error } = useProducts();
+
+    return(
+        <>
+        <h1>Hola</h1>
+        </>
+    )
+};
+
+export default Home;
