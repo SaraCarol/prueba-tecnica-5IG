f5ef6a1589ad54bd6005f09c542e99a780667c89
SaraCarol <mendoza.saracarolina@gmail.com>
2025-08-04 21:08:31 -0500

    feat: Agregando paginacion de productos

--- task-3/src/pages/Home.jsx
+++ task-3/src/pages/Home.jsx
@@ -1,30 +1,44 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { Card, Container, Grid, Box } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
 import { useProducts } from '../context/ProductContext';
 import CardProduct from '../components/CardProduct';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 const Home = () => {
     const { products, loading, error } = useProducts();
-    const getScreenSize = () => {
-        const width = window.innerWidth;
-        if (width < 600) return "mobile"; 
-        if (width >= 600 && width < 960) return "tablet"; 
-        return "desktop";
-    };
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const productsPerPage = 8;
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+    const totalPages = Math.ceil(products.length / productsPerPage);
     console.log(products)
 
     return(
-        <Container className={styles.contenedor}>
+        <Container className={styles.container}>
 
-            <Box className={styles.productos}>
-                {products.map( producto =>(
-                    <Box item key={producto.id} className={styles.contenidoCard}>
+        <Container className={styles.containerProducts}>
+            <Box className={styles.products}>
+                {currentProducts.map( producto =>(
+                    <Box item key={producto.id} className={styles.boxCard}>
                         <CardProduct producto={producto} />
                     </Box>
                 ))}
             </Box>
+            <Box className={styles.pagination} >
+                <Pagination
+                    count={totalPages}
+                    page={currentPage}
+                    onChange={(e, value) => setCurrentPage(value)}
+                    color="primary"
+                />
+            </Box>
+        </Container>
+            
         
         </Container>
     );
